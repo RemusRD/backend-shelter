bfbd26ea280c37e4ce21e49377ec519e0a8725c8
Remus Richard Dumitrache <Richard.dumitrache@personio.de>
2021-10-26 10:58:23 +0200

    Add multiple java services into docker-compose

--- src/main/java/com/example/backendshelter/provider/FactProvider.java
+++ src/main/java/com/example/backendshelter/provider/FactProvider.java
@@ -1,16 +1,22 @@
 package com.example.backendshelter.provider;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@RequiredArgsConstructor
 public class FactProvider {
     private final RestTemplate restTemplate;
+    private final String url;
+
+    public FactProvider(RestTemplate restTemplate,
+                        @Value("${shelter.cat.fact.provider.url}") String url) {
+        this.restTemplate = restTemplate;
+        this.url = url;
+    }
 
     public String getFact() {
-        final var response = restTemplate.getForEntity("https://catfact.ninja/fact", FactResponse.class);
+        final var response = restTemplate.getForEntity(url, FactResponse.class);
         return response.getBody().getFact();
     }
 }
